e4be665c26af51641c3135eeeb88637cce36c4d2
Nanu*121 <rsomeshnanu>
2023-02-13 12:51:34 +0530

    changes

--- src/main/java/com/slokam/sec/DemoApplication.java
+++ src/main/java/com/slokam/sec/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+//hai hello how are you
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
